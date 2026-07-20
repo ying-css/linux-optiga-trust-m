98839e718d9a216700f8754c51d172cd83545948
baolam <135998293+BaoLam194@users.noreply.github.com>
2026-07-20 17:00:11 +0800

    Remove mbedtls v2/3 support

--- ex_cli_applications/trustm_hmac_verify_Auth.c
+++ ex_cli_applications/trustm_hmac_verify_Auth.c
@@ -44,14 +44,8 @@
 #include "pal_os_memory.h"
 #include "pal_crypt.h"
 #include "mbedtls/version.h"
-
-#if defined(MBEDTLS_VERSION_NUMBER) && MBEDTLS_VERSION_NUMBER >= 0x04000000
 #include <psa/crypto.h>
-#else
-#include "mbedtls/ccm.h"
-#include "mbedtls/md.h"
-#include "mbedtls/ssl.h"
-#endif
+
 #define PSA_CRYPT_SHA256_SIZE           (32U)
 
 typedef struct _OPTFLAG {
@@ -108,7 +102,6 @@ static pal_status_t pal_crypt_hmac(pal_crypt_t* p_pal_crypt,
     }
 #endif // OPTIGA_LIB_DEBUG_NULL_CHECK
     do{
-#if defined(MBEDTLS_VERSION_NUMBER) && MBEDTLS_VERSION_NUMBER >= 0x04000000
         psa_status_t status;
         psa_key_attributes_t attr = PSA_KEY_ATTRIBUTES_INIT;
         psa_key_id_t key_id = 0;
@@ -147,19 +140,6 @@ static pal_status_t pal_crypt_hmac(pal_crypt_t* p_pal_crypt,
             break;
         }
         return_value = PAL_STATUS_SUCCESS;
-#else
-        const mbedtls_md_info_t * hmac_info;
-
-        if ((uint16_t)OPTIGA_HMAC_SHA_256 != hmac_type){
-            break;
-        }
-        hmac_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA256);
-
-        if (0 != mbedtls_md_hmac(hmac_info, secret_key, secret_key_len, input_data, input_data_length, hmac)){
-            break;
-        }
-        return_value = PAL_STATUS_SUCCESS;
-#endif
     } while(FALSE); 
     return return_value;
 }
--- trustm_installation_script.sh
+++ trustm_installation_script.sh
@@ -5,10 +5,8 @@ sudo apt -y install git gcc build-essential libssl-dev gpiod libgpiod-dev curl x
 
 
 # Configuration, choose the version of mbedTLS
-MBEDTLS_VARIANT=4
 set -e
 echo "-----> Build Trust M Linux Tools and provider"
-if [ "$MBEDTLS_VARIANT" -eq 4 ]; then
 (
     echo "-----> Generate mbedtls config files" 
     cd trustm_lib/external/mbedtls-4.x 
@@ -20,13 +18,13 @@ if [ "$MBEDTLS_VARIANT" -eq 4 ]; then
     python3 framework/scripts/make_generated_files.py
 )
 fi
-sudo make uninstall MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-make clean MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-make -j5 MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-sudo make install MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
+sudo make uninstall 
+make clean 
+make -j5 
+sudo make install 
 echo "-----> Build Protected Update Set tool"
 cd ex_protected_update_data_set/Linux/
-sudo make uninstall MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-make clean MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-make -j5 MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
-sudo make install MBEDTLS_VARIANT="$MBEDTLS_VARIANT"
+sudo make uninstall 
+make clean 
+make -j5 
+sudo make install 
